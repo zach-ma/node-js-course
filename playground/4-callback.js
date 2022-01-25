b79e4b3730d0014f51923386261f11977f61bc1e
Zach Ma <zachma@mbp.local>
2022-01-25 14:45:19 -0800

    MongoDB

--- playground/4-callback.js
+++ playground/4-callback.js
@@ -1,3 +1,17 @@
+const doWorkCallback = (callback) => {
+  setTimeout(() => {
+    // callback("This is my error!", undefined);
+    callback(undefined, [1, 4, 7]);
+  }, 2000);
+};
+
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(result);
+});
+
 // setTimeout(() => {
 //   console.log("Two seconds are up");
 // }, 2000);
@@ -29,12 +43,12 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
-const add = (a, b, callback) => {
-  setTimeout(() => {
-    callback(a + b);
-  }, 2000);
-};
+// const add = (a, b, callback) => {
+//   setTimeout(() => {
+//     callback(a + b);
+//   }, 2000);
+// };
 
-add(1, 4, (sum) => {
-  console.log(sum); // Should print: 5
-});
+// add(1, 4, (sum) => {
+//   console.log(sum); // Should print: 5
+// });
