6503430751e816b3d4ccde88b02c68512a5cef1b
Zach Ma <mjx.ma@icloud.com>
2022-03-08 15:01:22 -0800

    GET /planets, CORS middleware

---
+++ nasa-project/server/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const planetsRouter = require("./routes/planets/planets.router");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+  })
+); // cors middleware
+app.use(express.json()); // middleware: parse incoming json from incoming request
+app.use(planetsRouter);
+
+module.exports = app;
