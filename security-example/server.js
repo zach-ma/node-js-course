8010fd30b1a4d593b93e80780b0f117f1d230fae
Zach Ma <mjx.ma@icloud.com>
2022-03-12 19:04:04 -0800

    helmet.js

--- security-example/server.js
+++ security-example/server.js
@@ -3,10 +3,13 @@ const path = require("path");
 
 const https = require("https");
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get("/secret", (req, res) => {
   return res.send("Your personal secret value is 42!");
