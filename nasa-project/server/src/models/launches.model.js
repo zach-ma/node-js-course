6d4f6212baef7dd517155f77997c296496618893
Zach Ma <mjx.ma@icloud.com>
2022-03-12 16:50:26 -0800

    persisting SpaceX launches

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -1,28 +1,95 @@
+const axios = require("axios");
+
 const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-// const launches = new Map(); // state in memory
-// let latestFlightNumber = 100; // state in memory
-
 const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X", // required
-  rocket: "Explorer IS1", // required
-  launchDate: new Date("December 27, 2030"), // required
-  target: "Kepler-442 b", // required
-  customers: ["NASA", "SPACE X"],
-  upcoming: true,
-  success: true,
+  flightNumber: 100, // flight_number
+  mission: "Kepler Exploration X", // name
+  rocket: "Explorer IS1", // rocket.name
+  launchDate: new Date("December 27, 2030"), // date_local
+  target: "Kepler-442 b", // not applicable
+  customers: ["NASA", "SPACE X"], // payload.customers for each payload
+  upcoming: true, // upcoming
+  success: true, // success
 };
 
-// launches.set(launch.flightNumber, launch);
-// saveLaunch(launch);
+saveLaunch(launch);
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function populateLaunches() {
+  console.log("Downloading launch data...");
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  if (response.status !== 200) {
+    console.log("Problem downloading launch data");
+    throw new Error("Launch data download failed");
+  }
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+
+    // populate launches collection
+    await saveLaunch(launch);
+  }
+}
+
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+  if (firstLaunch) {
+    console.log("Launch data already loaded");
+  } else {
+    await populateLaunches();
+  }
+}
+
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
 
 async function existsLaunchWithId(launchId) {
-  // return launches.has(launchId);
-  return await launchesDatabase.findOne({ flightNumber: launchId });
+  return await findLaunch({ flightNumber: launchId });
 }
 
 async function getLatestFlightNumber() {
@@ -34,63 +101,42 @@ async function getLatestFlightNumber() {
 }
 
 async function getAllLaunches() {
-  //   return Array.from(launches.values());
   return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({ keplerName: launch.target });
-
-  try {
-    if (!planet) {
-      throw new Error("No matching planet found");
+  await launchesDatabase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true, // insert + update = upsert
     }
-
-    await launchesDatabase.findOneAndUpdate(
-      {
-        flightNumber: launch.flightNumber,
-      },
-      launch,
-      {
-        // insert + update = upsert
-        upsert: true,
-      }
-    );
-  } catch (err) {
-    console.error(`Could not save launch ${err}`);
-  }
+  );
 }
-
-// function addNewLaunch(launch) {
-//   latestFlightNumber++;
-//   launches.set(
-//     latestFlightNumber,
-//     Object.assign(launch, {
-//       flightNumber: latestFlightNumber,
-//       customers: ["NASA", "SPACE X"],
-//       upcoming: true,
-//       success: true,
-//     })
-//   );
-// }
 
 // increment latestFlightNumber by 1, assign default properties
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({ keplerName: launch.target });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
   const newLaunch = Object.assign(launch, {
     flightNumber: newFlightNumber,
     customers: ["NASA", "SPACE X"],
     upcoming: true,
     success: true,
   });
+
   await saveLaunch(newLaunch);
 }
 
 async function abortLaunchById(launchId) {
-  //   const aborted =  launches.get(launchId);
-  //   aborted.upcoming = false;
-  //   aborted.success = false;
-  //   return aborted;
   const aborted = await launchesDatabase.updateOne(
     {
       flightNumber: launchId,
@@ -105,9 +151,9 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+  loadLaunchData,
   existsLaunchWithId,
   getAllLaunches,
-  //   addNewLaunch,
   scheduleNewLaunch,
   abortLaunchById,
 };
--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -14,7 +14,7 @@ describe("Launches API", () => {
   describe("Test GET /launches", () => {
     test("It should respond with 200 success", async () => {
       const response = await request(app)
-        .get("/launches")
+        .get("/v1/launches")
         .expect("Content-Type", /json/)
         .expect(200);
       // expect(response.statusCode).toBe(200);
@@ -45,7 +45,7 @@ describe("Launches API", () => {
     test("It should respond with 201 created", async () => {
       // NOTE: SuperTest Expect
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(completeLaunchData)
         .expect("Content-Type", /json/)
         .expect(201);
@@ -61,7 +61,7 @@ describe("Launches API", () => {
 
     test("It should catch missing required properties", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithoutDate)
         .expect("Content-Type", /json/)
         .expect(400);
@@ -73,7 +73,7 @@ describe("Launches API", () => {
 
     test("It should catch invalid dates", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithInvalidDate)
         .expect("Content-Type", /json/)
         .expect(400);
